944bf2e4dccde8380dd8b3a9147fb38b12237fb7
Denisska1231 <jedthaphon@gmail.com>
2021-12-28 19:43:35 +0700

    Completed lab14_1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,20 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double a[],int b,double c[]){
+ double sum=0,SD=0,GM=1,HM=0,Max=a[0],Min=a[0];
+ for(int i=0;i<b;i++){
+     sum += a[i];
+     SD += pow(a[i],2);
+     GM *= a[i];
+     HM += 1/a[i];
+     if(a[i]>Max) Max=a[i];
+     if(a[i]<Min) Min=a[i];
+ }  
+    c[0] = sum/b;
+    c[1] = sqrt((SD/b)-pow(c[0],2));
+    c[2] = pow(GM,1.0/b);
+    c[3] = b/HM;
+    c[4] = Max;
+    c[5] = Min;
+}
